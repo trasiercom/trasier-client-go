59b9bc9412d1ad9952b3c30ea6c6fd92c81d3622
kreuzerk <kevin.kreuzer90@icloud.com>
2018-06-30 12:19:51 +0200

    Refactor getAuthToken code

--- client/authentication/auth.service.go
+++ client/authentication/auth.service.go
@@ -9,44 +9,60 @@ import (
 	"io/ioutil"
 )
 
+const keycloakUrl = "https://trasier-keycloak-test.app.trasier.com/auth/realms/trasier-dev/protocol/openid-connect/token"
+
 type AuthService struct {
 	clientId     string
 	clientSecret string
+	request      *http.Request
 }
 
 func NewAuthService(clientId string, clientSecret string) *AuthService {
-	return &AuthService{clientId: clientId, clientSecret: clientSecret}
+	request, err := createRequest(clientId, clientSecret)
+	if err != nil {
+		log.Fatal("Something went wrong while creating the request", err)
+	}
+	return &AuthService{clientId: clientId, clientSecret: clientSecret, request: request}
+}
+
+func createRequest(clientId string, clientSecret string) (request *http.Request, err error) {
+	request, err = http.NewRequest("POST", keycloakUrl, strings.NewReader(createRequestBody().Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	request.SetBasicAuth(clientId, clientSecret)
+	return
+}
+
+func createRequestBody() (requestBody url.Values) {
+	requestBody = url.Values{}
+	requestBody.Set("grant_type", "client_credentials")
+	requestBody.Add("scope", "")
+	return
 }
 
 func (authService *AuthService) GetAuthToken() (*AuthToken) {
-	client := &http.Client{}
+	body := authService.requestToken()
+	return authService.convertToAuthToken(body)
+}
 
-	data := url.Values{}
-	data.Set("grant_type", "client_credentials")
-	data.Add("scope", "")
-
-	req, err := http.NewRequest("POST", "https://trasier-keycloak-test.app.trasier.com/auth/realms/trasier-dev/protocol/openid-connect/token", strings.NewReader(data.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.SetBasicAuth(authService.clientId, authService.clientSecret)
-
-	resp, err := client.Do(req)
-
+func (authService *AuthService) convertToAuthToken(body []byte) *AuthToken {
+	authToken := new(AuthToken)
+	err := json.Unmarshal(body, &authToken)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Something went wrong during unmarshalling the response", err.Error())
+	}
+	return authToken
+}
+
+func (authService *AuthService) requestToken() []byte {
+	client := &http.Client{}
+	resp, err := client.Do(authService.request)
+	if err != nil {
+		log.Fatal("Something went wrong while sending the request", err)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
-
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal("Something went wrong whily reading the response", err.Error())
 	}
-
-	authToken := new(AuthToken)
-	err = json.Unmarshal(body, &authToken)
-
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	return authToken
+	return body
 }
