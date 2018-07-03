aa053bb4e7331865af180157a3900b1c6ee995f6
kreuzerk <kevin.kreuzer90@icloud.com>
2018-07-03 21:27:58 +0200

    Adjust mainfile

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"./example"
+	"./client/authentication"
 )
 
 func main() {
+
+	authService := authentication.NewAuthService("trasier-dev_170530_demo1", "MjUxYzRmNDAtMWJiOS00NzhiLWEzMWYtYWQxNWRkZGVmZTg4")
+
+	println("Getting the token")
+	println("=====================")
+	authToken := authService.GetToken()
+	println("Auth token: " + authToken.AccessToken)
+	println("=====================")
+
 	awesomeConsumer := example.ExampleConsumer{}
 	awesomeConsumer.SendSpans()
 }
